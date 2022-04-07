02d110ee596463fea44a2aec8a28670d0e68d17e
ItsmeLee <arsmagica90@gmail.com>
2022-04-07 13:39:42 +0900

    Next와 TS설치 및 기초 셋업

---
+++ pages/_app.tsx
@@ -0,0 +1,22 @@
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import { ThemeProvider } from "styled-components";
+import { GlobalStyle } from "../styles/global-style";
+//import { theme } from "../styles/theme";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Blog.DEV</title>
+      </Head>
+      <GlobalStyle />
+      <Component {...pageProps} />
+      {/*       <ThemeProvider theme={theme}>
+      </ThemeProvider> */}
+    </>
+  );
+}
+
+export default MyApp;
---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+
+const Home: NextPage = () => {
+  return <h1>This is Home</h1>;
+};
+
+export default Home;
